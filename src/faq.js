8159a7de71a1ca777daee24924dd095907c46b8b
cbethax <cbethax@gmail.com>
2016-02-27 15:18:29 +0200

    finished faqs

--- src/faq.js
+++ src/faq.js
@@ -6,11 +6,24 @@ export class Faq {
     var item = $('.faq .list-item[data-faq-item="' + i + '"] .content');
 
     if(!item.hasClass("show")) {
-      $('.faq .list-item .content').removeClass("show")
+      if($('.faq .list-item .content').hasClass("show")) {
+        $('.faq .list-item .content').removeClass("show")
+        $('.faq .list-item .title i').removeClass("fa-angle-down").addClass("fa-angle-right");
 
-      item.addClass("show");
+        setTimeout(function() {
+          item.addClass("show");
+          $('.faq .list-item[data-faq-item="' + i + '"] .title i').removeClass("fa-angle-right").addClass("fa-angle-down");
+        }, 650);
+      } else {
+        $('.faq .list-item .content').removeClass("show")
+        $('.faq .list-item .title i').removeClass("fa-angle-down").addClass("fa-angle-right");
+
+        item.addClass("show");
+        $('.faq .list-item[data-faq-item="' + i + '"] .title i').removeClass("fa-angle-right").addClass("fa-angle-down");
+      }
     } else {
       $('.faq .list-item .content').removeClass("show")
+      $('.faq .list-item .title i').removeClass("fa-angle-down").addClass("fa-angle-right");
     }
   }
 }
